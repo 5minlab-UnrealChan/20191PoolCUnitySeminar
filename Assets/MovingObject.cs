0859efb6964332ff38add101e7d44d33368c50fc
HBJ <bomul0327@naver.com>
2019-04-06 14:37:41 +0900

    1주차 + 2주차 일부

---
+++ Assets/MovingObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObject : MonoBehaviour
+{
+    private float time =0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time = Mathf.PingPong(Time.time, 1f);
+        Debug.Log(time);
+        transform.position = Vector3.Lerp(new Vector3(-1.25f, transform.position.y, transform.position.z), new Vector3(5.25f, transform.position.y, transform.position.z), time);
+    
+    }
+}
---
+++ Assets/Scripts/NavigationTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavigationTest : MonoBehaviour
+{
+    public Transform[] positions;
+
+    private NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(positions[Random.Range(0, positions.Length)].position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A)){
+            agent.SetDestination(positions[Random.Range(0, positions.Length)].position);
+        }
+    }
+}
